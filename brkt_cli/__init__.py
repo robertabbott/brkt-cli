robertabbott/brkt-cli

commit 23641a81b6804762c4b68ab6f2005cc53fdf4a02

Detect duplicate AMI name
When --encrypted-ami-name is specified, see if there's already an AMI
with the given name before proceeding.  Move a bunch of validation code
into a separate function, so that it's called from both
command_encrypt_ami() and command_update_encrypted_ami().  Also, we can
now unit test the validation code.

brkt_cli/__init__.py
@@ -62,25 +62,61 @@ def _validate_subnet_and_security_groups(aws_svc,
     return len(vpc_ids) <= 1
 
 
-def command_encrypt_ami(values, log):
-    region = values.region
+class ValidationError(Exception):
+    pass
 
+
+def _connect_and_validate(aws_svc, values, encryptor_ami_id):
+    """ Connect to the AWS service and validate command-line options
+
+    :param aws_svc: the BaseAWSService implementation
+    :param values: object that was generated by argparse
+    """
     if values.encrypted_ami_name:
         try:
             aws_service.validate_image_name(values.encrypted_ami_name)
         except aws_service.ImageNameError as e:
-            print(e.message, file=sys.stderr)
-            return 1
+            raise ValidationError(e.message)
 
-    # Validate the region.
-    regions = [str(r.name) for r in boto.vpc.regions()]
-    if region not in regions:
-        print(
+    regions = [str(r.name) for r in aws_svc.get_regions()]
+    if values.region not in regions:
+        raise ValidationError(
             'Invalid region %s.  Must be one of %s.' %
-            (region, str(regions)),
-            file=sys.stderr
-        )
-        return 1
+            (values.region, str(regions)))
+
+    aws_svc.connect(values.region, key_name=values.key_name)
+
+    try:
+        if values.key_name:
+            aws_svc.get_key_pair(values.key_name)
+
+        if not _validate_subnet_and_security_groups(
+                aws_svc, values.subnet_id, values.security_group_ids):
+            raise ValidationError(
+                    'Subnet and security groups must be in the same VPC')
+
+        if not values.no_validate_ami:
+            error_msg = aws_svc.validate_guest_ami(values.ami)
+            if error_msg:
+                raise ValidationError(error_msg)
+
+            error_msg = aws_svc.validate_encryptor_ami(encryptor_ami_id)
+            if error_msg:
+                raise ValidationError(error_msg)
+
+        if values.encrypted_ami_name:
+            filters = {'name': values.encrypted_ami_name}
+            if aws_svc.get_images(filters=filters):
+                raise ValidationError(
+                        'There is already an image named %s' %
+                        values.encrypted_ami_name
+                )
+    except EC2ResponseError as e:
+        raise ValidationError(e.message)
+
+
+def command_encrypt_ami(values, log):
+    region = values.region
 
     encryptor_ami = values.encryptor_ami
     if not encryptor_ami:
@@ -93,128 +129,42 @@ def command_encrypt_ami(values, log):
     session_id = util.make_nonce()
     default_tags = encrypt_ami.get_default_tags(session_id, encryptor_ami)
 
-    try:
-        # Connect to AWS.
-        aws_svc = aws_service.AWSService(
-            session_id, default_tags=default_tags)
-        aws_svc.connect(region, key_name=values.key_name)
-    except NoAuthHandlerFound:
-        msg = (
-            'Unable to connect to AWS.  Are your AWS_ACCESS_KEY_ID and '
-            'AWS_SECRET_ACCESS_KEY environment variables set?'
-        )
-        if values.verbose:
-            log.exception(msg)
-        else:
-            log.error(msg)
-        return 1
+    aws_svc = aws_service.AWSService(
+        session_id, default_tags=default_tags)
+    _connect_and_validate(aws_svc, values, encryptor_ami)
 
-    try:
-        if values.key_name:
-            # Validate the key pair name.
-            aws_svc.get_key_pair(values.key_name)
+    log.info('Starting encryptor session %s', aws_svc.session_id)
 
-        if not _validate_subnet_and_security_groups(
-            aws_svc,
-            subnet_id=values.subnet_id,
-            security_group_ids=values.security_group_ids
-        ):
-            log.error('Subnet and security groups must be in the same VPC.')
-            return 1
-
-        if not values.no_validate_ami:
-            error = aws_svc.validate_guest_ami(values.ami)
-            if error:
-                print(error, file=sys.stderr)
-                return 1
-
-            error = aws_svc.validate_encryptor_ami(encryptor_ami)
-            if error:
-                print(error, file=sys.stderr)
-                return 1
-
-        log.info('Starting encryptor session %s', aws_svc.session_id)
-
-        encrypted_image_id = encrypt_ami.encrypt(
-            aws_svc=aws_svc,
-            enc_svc_cls=encryptor_service.EncryptorService,
-            image_id=values.ami,
-            encryptor_ami=encryptor_ami,
-            encrypted_ami_name=values.encrypted_ami_name,
-            subnet_id=values.subnet_id,
-            security_group_ids=values.security_group_ids,
-            brkt_env=values.brkt_env
-        )
-        # Print the AMI ID to stdout, in case the caller wants to process
-        # the output.  Log messages go to stderr.
-        print(encrypted_image_id)
-        return 0
-    except EC2ResponseError as e:
-        if e.error_code == 'AuthFailure':
-            msg = 'Check your AWS login credentials and permissions'
-            if values.verbose:
-                log.exception(msg)
-            else:
-                log.error(msg + ': ' + e.error_message)
-        elif e.error_code in (
-                'InvalidKeyPair.NotFound',
-                'InvalidSubnetID.NotFound',
-                'InvalidGroup.NotFound'):
-            if values.verbose:
-                log.exception(e.error_message)
-            else:
-                log.error(e.error_message)
-        elif e.error_code == 'UnauthorizedOperation':
-            if values.verbose:
-                log.exception(e.error_message)
-            else:
-                log.error(e.error_message)
-            log.error(
-                'Unauthorized operation.  Check the IAM policy for your '
-                'AWS account.'
-            )
-        else:
-            raise
-    except util.BracketError as e:
-        if values.verbose:
-            log.exception(e.message)
-        else:
-            log.error(e.message)
-    except KeyboardInterrupt:
-        if values.verbose:
-            log.exception('Interrupted by user')
-        else:
-            log.error('Interrupted by user')
-    return 1
+    encrypted_image_id = encrypt_ami.encrypt(
+        aws_svc=aws_svc,
+        enc_svc_cls=encryptor_service.EncryptorService,
+        image_id=values.ami,
+        encryptor_ami=encryptor_ami,
+        encrypted_ami_name=values.encrypted_ami_name,
+        subnet_id=values.subnet_id,
+        security_group_ids=values.security_group_ids,
+        brkt_env=values.brkt_env
+    )
+    # Print the AMI ID to stdout, in case the caller wants to process
+    # the output.  Log messages go to stderr.
+    print(encrypted_image_id)
+    return 0
 
 
 def command_update_encrypted_ami(values, log):
-    if values.encrypted_ami_name:
-        try:
-            aws_service.validate_image_name(values.encrypted_ami_name)
-            encrypted_ami_name = values.encrypted_ami_name
-        except aws_service.ImageNameError as e:
-            print(e.message, file=sys.stderr)
-            return 1
-    else:
-        encrypted_ami_name = None
     region = values.region
     nonce = util.make_nonce()
     default_tags = encrypt_ami.get_default_tags(nonce, '')
     aws_svc = aws_service.AWSService(
         nonce, default_tags=default_tags)
-    if not aws_svc.validate_region(region):
-        print ('Invalid region %s' % region,
-               file=sys.stderr)
-        return 1
-    aws_svc.connect(region, key_name=values.key_name)
+    _connect_and_validate(aws_svc, values.updater_ami)
+
     encrypted_ami = values.ami
     if not values.no_validate_ami:
         guest_ami_error = aws_svc.validate_guest_encrypted_ami(encrypted_ami)
         if guest_ami_error:
-            print ('Encrypted AMI verification failed: %s' % guest_ami_error,
-                   file=sys.stderr)
-            return 1
+            raise ValidationError(
+                'Encrypted AMI verification failed: %s' % guest_ami_error)
     else:
         log.info('skipping AMI verification')
     updater_ami = values.updater_ami
@@ -297,10 +247,60 @@ def main():
     log.setLevel(log_level)
     aws_service.log.setLevel(log_level)
     encryptor_service.log.setLevel(log_level)
-    if values.subparser_name == 'encrypt-ami':
-        return command_encrypt_ami(values, log)
-    if values.subparser_name == 'update-encrypted-ami':
-        return command_update_encrypted_ami(values, log)
+
+    try:
+        if values.subparser_name == 'encrypt-ami':
+            return command_encrypt_ami(values, log)
+        if values.subparser_name == 'update-encrypted-ami':
+            return command_update_encrypted_ami(values, log)
+    except ValidationError as e:
+        print(e, file=sys.stderr)
+    except NoAuthHandlerFound:
+        msg = (
+            'Unable to connect to AWS.  Are your AWS_ACCESS_KEY_ID and '
+            'AWS_SECRET_ACCESS_KEY environment variables set?'
+        )
+        if values.verbose:
+            log.exception(msg)
+        else:
+            log.error(msg)
+    except EC2ResponseError as e:
+        if e.error_code == 'AuthFailure':
+            msg = 'Check your AWS login credentials and permissions'
+            if values.verbose:
+                log.exception(msg)
+            else:
+                log.error(msg + ': ' + e.error_message)
+        elif e.error_code in (
+                'InvalidKeyPair.NotFound',
+                'InvalidSubnetID.NotFound',
+                'InvalidGroup.NotFound'):
+            if values.verbose:
+                log.exception(e.error_message)
+            else:
+                log.error(e.error_message)
+        elif e.error_code == 'UnauthorizedOperation':
+            if values.verbose:
+                log.exception(e.error_message)
+            else:
+                log.error(e.error_message)
+            log.error(
+                'Unauthorized operation.  Check the IAM policy for your '
+                'AWS account.'
+            )
+        else:
+            raise
+    except util.BracketError as e:
+        if values.verbose:
+            log.exception(e.message)
+        else:
+            log.error(e.message)
+    except KeyboardInterrupt:
+        if values.verbose:
+            log.exception('Interrupted by user')
+        else:
+            log.error('Interrupted by user')
+    return 1
 
 
 if __name__ == '__main__':

brkt_cli/aws_service.py
@@ -38,6 +38,14 @@ class BaseAWSService(object):
         self.session_id = session_id
 
     @abc.abstractmethod
+    def get_regions(self):
+        pass
+
+    @abc.abstractmethod
+    def connect(self, region, key_name=None):
+        pass
+
+    @abc.abstractmethod
     def run_instance(self,
                      image_id,
                      security_group_ids=None,
@@ -121,6 +129,10 @@ class BaseAWSService(object):
 
     @abc.abstractmethod
     def get_image(self, image_id):
+        pass
+
+    @abc.abstractmethod
+    def get_images(self, filters=None):
         pass
 
     @abc.abstractmethod
@@ -238,11 +250,8 @@ class AWSService(BaseAWSService):
         self.region = None
         self.conn = None
 
-    def validate_region(self, region):
-        regions = [str(r.name) for r in boto.vpc.regions()]
-        if region not in regions:
-            return False
-        return True
+    def get_regions(self):
+        return boto.vpc.regions()
 
     def connect(self, region, key_name=None):
         self.region = region
@@ -438,6 +447,9 @@ class AWSService(BaseAWSService):
             root_device_name='/dev/sda1',
             virtualization_type='paravirtual'
         )
+
+    def get_images(self, filters=None):
+        return self.conn.get_all_images(filters=filters)
 
     @retry_boto(error_code_regexp=r'InvalidAMIID\.NotFound')
     def get_image(self, image_id):

test.py
@@ -14,6 +14,7 @@
 from boto.ec2.keypair import KeyPair
 from boto.ec2.securitygroup import SecurityGroup
 from boto.exception import EC2ResponseError
+from boto.regioninfo import RegionInfo
 from boto.vpc import Subnet
 
 import brkt_cli
@@ -88,10 +89,17 @@ class DummyAWSService(aws_service.BaseAWSService):
         self.tagged_volumes = []
         self.subnets = {}
         self.security_groups = {}
+        self.regions = [RegionInfo(name='us-west-2')]
 
         # Callbacks.
         self.run_instance_callback = None
         self.create_security_group_callback = None
+
+    def get_regions(self):
+        return self.regions
+
+    def connect(self, region, key_name=None):
+        pass
 
     def run_instance(self,
                      image_id,
@@ -224,7 +232,8 @@ class DummyAWSService(aws_service.BaseAWSService):
         pass
 
     def validate_encryptor_ami(self, ami_id):
-        pass
+        if not ami_id or not ami_id.startswith('ami-'):
+            raise Exception('Invalid AMI ID: %s', ami_id)
 
     def register_image(self,
                        kernel_id,
@@ -232,7 +241,7 @@ class DummyAWSService(aws_service.BaseAWSService):
                        name=None,
                        description=None):
         image = Image()
-        image.id = _new_id()
+        image.id = 'ami-' + _new_id()
         image.block_device_mapping = block_device_map
         image.state = 'available'
         image.name = name
@@ -246,6 +255,16 @@ class DummyAWSService(aws_service.BaseAWSService):
 
     def get_image(self, image_id):
         return self.images[image_id]
+
+    def get_images(self, filters=None):
+        # Only filtering by name is currently supported.
+        name = filters.get('name', None)
+        images = []
+        if name:
+            for i in self.images.values():
+                if i.name == name:
+                    images.append(i)
+        return images
 
     def delete_snapshot(self, snapshot_id):
         del(self.snapshots[snapshot_id])
@@ -678,7 +697,20 @@ class TestInstance(unittest.TestCase):
             self.assertTrue('unexpectedly terminated' in e.message)
 
 
-class TestEncryptCommand(unittest.TestCase):
+class DummyValues(object):
+
+    def __init__(self):
+        self.encrypted_ami_name = None
+        self.region = 'us-west-2'
+        self.key_name = None
+        self.subnet_id = None
+        self.security_group_ids = []
+        self.no_validate_ami = False
+        self.ami = None
+        self.encryptor_ami = None
+
+
+class TestValidation(unittest.TestCase):
 
     def test_validate_subnet_and_security_groups(self):
         aws_svc, encryptor_image, guest_image = _build_aws_service()
@@ -709,3 +741,26 @@ class TestEncryptCommand(unittest.TestCase):
         self.assertFalse(brkt_cli._validate_subnet_and_security_groups(
             aws_svc, subnet_id=subnet.id, security_group_ids=[sg3.id]
         ))
+
+    def test_duplicate_image_name(self):
+        """ Test that we detect name collisions with the encrypted image.
+        """
+        aws_svc, encryptor_image, guest_image = _build_aws_service()
+
+        # No name.
+        values = DummyValues()
+        image = None
+        for image_id, i in aws_svc.images.iteritems():
+            values.ami = image_id
+            image = i
+        brkt_cli._connect_and_validate(aws_svc, values, encryptor_image.id)
+
+        # Unique name.
+        image.name = 'My image'
+        values.encrypted_ami_name = 'Propsed name'
+        brkt_cli._connect_and_validate(aws_svc, values, encryptor_image.id)
+
+        # Name collision.
+        values.encrypted_ami_name = image.name
+        with self.assertRaises(brkt_cli.ValidationError):
+            brkt_cli._connect_and_validate(aws_svc, values, encryptor_image.id)

commit d60ae8eba5ce07ab2b2e2c1d158522a86ff65a19

Fix up update-encrypted-ami code
Fix breakage in the update-encrypted-ami code that was introduced by
commit 23641a81b6804762c4b68ab6f2005cc53fdf4a02.  Add hidden --security-
group and --subnet arguments.  These are hidden in the short term until
we implement them for update-encrypted-ami, so that the validation code
doesn't break.  Call encrypt_ami.clean_up(), to better handle errors
when cleaning up resources.

brkt_cli/__init__.py
@@ -157,7 +157,7 @@ def command_update_encrypted_ami(values, log):
     default_tags = encrypt_ami.get_default_tags(nonce, '')
     aws_svc = aws_service.AWSService(
         nonce, default_tags=default_tags)
-    _connect_and_validate(aws_svc, values.updater_ami)
+    _connect_and_validate(aws_svc, values, values.updater_ami)
 
     encrypted_ami = values.ami
     if not values.no_validate_ami:
@@ -178,7 +178,8 @@ def command_update_encrypted_ami(values, log):
         mv_image.virtualization_type):
         log.error("Encryptor virtualization_type mismatch")
         return 1
-    if not values.encrypted_ami_name:
+    encrypted_ami_name = values.encrypted_ami_name
+    if not encrypted_ami_name:
         # Replace nonce in AMI name
         name = guest_image.name
         m = re.match('(\S+) \(encrypted (\S+)\)', name)

brkt_cli/update_ami.py
@@ -30,9 +30,9 @@ from brkt_cli import encryptor_service
 from brkt_cli.util import Deadline
 
 from encrypt_ami import (
+    clean_up,
     create_encryptor_security_group,
     log_exception_console,
-    terminate_instance,
     wait_for_instance,
     wait_for_snapshots,
     wait_for_volume,
@@ -56,7 +56,6 @@ def update_ami(aws_svc, encrypted_ami, updater_ami,
     updater = None
     mv_root_id = None
     temp_sg_id = None
-    tids = set()
     try:
         guest_image = aws_svc.get_image(encrypted_ami)
 
@@ -107,9 +106,15 @@ def update_ami(aws_svc, encrypted_ami, updater_ami,
 
         wait_for_instance(aws_svc, updater.id, state="running")
         updater.update()
-        host_ip = updater.ip_address
-        enc_svc = encryptor_service.EncryptorService(host_ip)
-        log.info("Waiting for updater service on %s" % (host_ip,))
+        host_ips = []
+        if updater.ip_address:
+            host_ips.append(updater.ip_address)
+        if updater.private_ip_address:
+            host_ips.append(updater.private_ip_address)
+
+        enc_svc = encryptor_service.EncryptorService(host_ips)
+        log.info('Waiting for updater service on %s (port %s on %s)',
+                 updater.id, enc_svc.port, ', '.join(host_ips))
         wait_for_encryptor_up(enc_svc, Deadline(600))
         try:
             wait_for_encryption(enc_svc)
@@ -204,25 +209,24 @@ def update_ami(aws_svc, encrypted_ami, updater_ami,
             block_device_mapping=guest_bdm
         )
         log.info("Created %s" % (ami,))
-        # Step 8. Clean up
-        terminate_instance(aws_svc, encrypted_guest.id, 'guest', tids)
-        terminate_instance(aws_svc, updater.id, 'updater', tids)
-        encrypted_guest = None
-        updater = None
-        aws_svc.delete_volume(mv_root_id)
-        mv_root_id = None
-        if temp_sg_id:
-            aws_svc.delete_security_group(temp_sg_id)
-        temp_sg_id = None
         return 0
     except:
         log.exception('Upgrade Failed')
     finally:
+        instance_ids = set()
+        volume_ids = set()
+        sg_ids = set()
+
         if encrypted_guest:
-            terminate_instance(aws_svc, encrypted_guest.id, 'guest', tids)
+            instance_ids.add(encrypted_guest.id)
         if updater:
-            terminate_instance(aws_svc, updater.id, 'updater', tids)
+            instance_ids.add(updater.id)
         if mv_root_id:
-            aws_svc.delete_volume(mv_root_id)
+            volume_ids.add(mv_root_id)
         if temp_sg_id:
-            aws_svc.delete_security_group(temp_sg_id)
+            sg_ids.add(temp_sg_id)
+
+        clean_up(aws_svc,
+                 instance_ids=instance_ids,
+                 volume_ids=volume_ids,
+                 security_group_ids=sg_ids)

brkt_cli/update_encrypted_ami_args.py
@@ -36,6 +36,22 @@ def setup_update_encrypted_ami(parser):
         help="Don't validate encrypted AMI properties"
     )
 
+    # These are temporarily hidden, so that the validation code works
+    # before we properly support security group and subnet for image update.
+    parser.add_argument(
+        '--security-group',
+        metavar='ID',
+        dest='security_group_ids',
+        action='append',
+        help=argparse.SUPPRESS
+    )
+    parser.add_argument(
+        '--subnet',
+        metavar='ID',
+        dest='subnet_id',
+        help=argparse.SUPPRESS
+    )
+
     # Optional yeti endpoints. Hidden because it's only used for development
     parser.add_argument(
         '--brkt-env',